ad0dbfc9e14fcdbc8ba733235c1fb528c5642218
ThGnommy <th.brandoli@gmail.com>
2021-10-06 15:22:37 +0200

    fix styles

--- utils.js
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-
-export const isDesktop = dynamic(
-  () => {
-    console.log("dinamyc imoport");
-    return window.matchMedia("(min-width: 1300px)");
-  },
-  { ssr: false }
-);
